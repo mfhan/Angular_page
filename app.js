09eeef3fd43ea3a2d27f91bcb64b5889fa0bda90
mfrance <mf212mf@gmail.com>
2016-04-04 14:22:42 -0400

    first GitHub commit

--- app.js
+++ app.js
@@ -13,11 +13,12 @@ var api = require('./routes/api');
 
 var uristring = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/'+appName;
 
-// Makes connection asynchronously.  Mongoose will queue up database operations and release them when the connection is complete.
+// Makes connection asynchronously.
+//Mongoose will queue up database operations and release them when the connection is complete.
 mongoose.connect(uristring, function (err, res) {
   if (err)
     console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-  else 
+  else
     console.log ('Succeeded connected to: ' + uristring);
 });
 
